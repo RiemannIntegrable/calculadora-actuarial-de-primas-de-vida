7463bd6377f2a6cecd63e56f98f84f5a68dc2667
RiemannIntegrable <jmacunahz.01@gmail.com>
2025-06-05 18:40:11 -0500

    commit

--- src/utils/aritmetica.R
+++ src/utils/aritmetica.R
@@ -1,4 +1,13 @@
-aritmetica <- function(tabla_mortalidad,r,i,x) {
-  w <- nrow(tabla_mortalidad) - 1
-  
+source("basics.R")
+
+IA <- function(tabla_mortalidad,r,i,x) {
+    w <- max(tabla_mortalidad$x)
+    if(w-x <= 0) return(0)
+
+    k_vals <= 0:(w-x)
+    for(k in k_vals) {
+        a
+    }
+
+    return(IrAx)
 }
--- src/utils/conmutaciones.R
+++ src/utils/conmutaciones.R
@@ -1,7 +1,8 @@
 D <- function(tabla_mortalidad, x, i){
-    
-}
 
-M <- function(tabla_mortalidad, x){
-    a
+    v <- 1/(1+i) 
+    lx <- tabla_mortalidad[tabla_mortalidad$x == x, "lx"]
+    Dx <- (v**x) * lx
+    
+    return(Dx)
 }
