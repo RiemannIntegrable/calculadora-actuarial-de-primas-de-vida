4e55561a103b4bc8566ca8157888062c0e09819f
Jefferson443 <yrubiom@unal.edu.co>
2025-06-05 18:38:51 -0500

    Check functions

--- src/utils/basics.R
+++ src/utils/basics.R
@@ -1,17 +1,19 @@
 # Funcion px
 p_x <- function(Edad, Tabla_mortalidad) {
-qx <- Tabla_mortalidad[which(Tabla_mortalidad$x==Edad), "qx"]
-    px <- 1-qx
-    return(px)
+  qx <- Tabla_mortalidad[which(Tabla_mortalidad$x==Edad), "qx"]
+  px <- 1-qx
+  return(px)
 }
 
 t_p_x <- function(Edad, Tiempo, Tabla_mortalidad) {
-    # Pre-asignar vector con longitud conocida
-p_vec <- numeric(Tiempo-1)  # Vector de ceros
-for(i in 1:n) {
-    qx <- Tabla_mortalidad[which(Tabla_mortalidad$x== (Edad+i)), "qx"]
+  # Pre-asignar vector con longitud conocida
+  p_vec <- numeric(Tiempo)  # Vector de ceros
+  for(i in 1:(Tiempo)) {
+    
+    qx <- Tabla_mortalidad[which(Tabla_mortalidad$x== (Edad+i-1)), "qx"]
     p_vec[i] <- 1- qx
-}
-  t_q_x <- prod(p_vec)
-    return(t_q_x)
+  }
+  # Productoria de p_vec
+  t_q_x <- prod(unlist(p_vec))
+  return(t_q_x)
 }
